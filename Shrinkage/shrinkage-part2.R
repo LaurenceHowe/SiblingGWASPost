ab873cedc18c7d8ccdc93543cf02f33b112e9ac9
LaurenceHowe <51400886+LaurenceHowe@users.noreply.github.com>
2020-06-01 14:50:20 +0100

    Create shrinkage-part2.R

---
+++ Shrinkage/shrinkage-part2.R
@@ -0,0 +1,58 @@
+require(data.table)
+require(metafor)
+require(msm)
+
+arguments <- commandArgs(trailingOnly = T)
+sumstatsfile <- arguments[1]
+phenotype <- arguments[2]
+set <- as.numeric(arguments[3])
+
+sumstats <- fread(sumstatsfile)
+print(sumstatsfile)
+
+if (set == 1) {
+filename <- paste(,phenotype,"_GWS.txt", sep = "")
+snps <- fread(filename, h = T)
+print("GWS")
+}
+
+if (set == 2) {
+filename <- paste(,phenotype,"_05.txt", sep = "")
+snps <- fread(filename, h = T)
+print("10-5")
+}
+
+
+merge <- merge(sumstats, snps,by = c("CHR", "BP"))
+print(nrow(merge))
+
+merge$Match[merge$A1 == merge$ALLELE1] <- 1
+merge$Match[merge$A1 == merge$ALLELE0] <- 0
+
+merge <- merge[! is.na(merge$Match), ]
+
+merge$BETA_TOTAL_H[merge$A1 == merge$ALLELE1] <- merge$BETA_TOTAL[merge$A1 == merge$ALLELE1]
+merge$BETA_TOTAL_H[merge$A1 == merge$ALLELE0] <- (-1)*merge$BETA_TOTAL[merge$A1 == merge$ALLELE0]
+merge$TOTAL_TOP <- merge$BETA * merge$BETA_TOTAL_H * (1/merge$SE_BETA_TOTAL^2)
+merge$TOTAL_BOT <- merge$BETA^2 * (1/merge$SE_BETA_TOTAL^2)
+
+
+IVWBeta_TOTAL <- sum(merge$TOTAL_TOP) / sum(merge$TOTAL_BOT)
+IVWSE_TOTAL <- 1 / sum(merge$TOTAL_BOT)
+print(c(IVWBeta_TOTAL, IVWSE_TOTAL))
+
+merge$BETA_WF_H[merge$A1 == merge$ALLELE1] <- merge$BETA_WF[merge$A1 == merge$ALLELE1]
+merge$BETA_WF_H[merge$A1 == merge$ALLELE0] <- (-1)*merge$BETA_WF[merge$A1 == merge$ALLELE0]
+merge$WF_TOP <- merge$BETA * merge$BETA_WF_H * (1/merge$SE_BETA_WF^2)
+merge$WF_BOT <- merge$BETA^2 * (1/merge$SE_BETA_WF^2)
+
+IVWBeta_WF <- sum(merge$WF_TOP) / sum(merge$WF_BOT)
+IVWSE_WF <- sqrt(1 / sum(merge$WF_BOT))
+print(c(IVWBeta_WF, IVWSE_WF))
+
+Ratio <- IVWBeta_WF / IVWBeta_TOTAL
+
+cov_matrix <- matrix(c(IVWSE_WF^2, 0, 0, IVWSE_TOTAL^2), nrow=2, ncol=2)
+RatioSE <- deltamethod(g=~(x1)/x2, mean=c(IVWBeta_WF, IVWBeta_TOTAL), cov=cov_matrix)
+
+print(c(1-Ratio, 1-(Ratio + 1.96*RatioSE), 1-(Ratio - 1.96*RatioSE)))
